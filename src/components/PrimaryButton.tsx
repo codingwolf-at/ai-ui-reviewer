fd61ccebba584e9fdafaa29314a2ca8e28e0593c
Atul Tameshwari <atameshwari9@gmail.com>
2026-01-21 13:31:33 +0530

    Add Spinner component and update PrimaryButton to use it for loading state. Enhance button layout for better alignment and user experience.

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Spinner from "./Spinner";
+
 type PrimaryButtonProps = {
     loading?: boolean;
     disabled?: boolean;
@@ -17,23 +19,16 @@ const PrimaryButton = ({
 }: PrimaryButtonProps) => {
     const isDisabled = disabled || loading;
 
-    // TODO: change loading animation, use spinner instead
-
     return (
         <button
             onClick={onClick}
             disabled={isDisabled}
-            className="bg-(--primary-btn) text-(--primary-btn-text) rounded-lg px-5 py-2.5 font-medium hover:opacity-90 active:scale-[0.98] cursor-pointer transition-all disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:opacity-50 flex justify-center gap-2"
+            className="bg-(--primary-btn) text-(--primary-btn-text) rounded-lg px-5 py-2.5 font-medium hover:opacity-90 active:scale-[0.98] cursor-pointer transition-all disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:opacity-50 flex items-center justify-center gap-2"
         >
             {loading ? (
                 <>
+                    <Spinner />
                     <span className="opacity-90">{loadingText}</span>
-                    <span className="flex items-center gap-1 ml-1">
-                        <span className="w-2 h-2 bg-black rounded-full animate-bounce [animation-delay:-0.3s]" />
-                        <span className="w-2 h-2 bg-black rounded-full animate-bounce [animation-delay:-0.15s]" />
-                        <span className="w-2 h-2 bg-black rounded-full animate-bounce" />
-                        <span className="w-2 h-2 bg-black rounded-full animate-bounce" />
-                    </span>
                 </>
             ) : (
                 children
